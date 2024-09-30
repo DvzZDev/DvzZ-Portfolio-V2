8d1e1f70dd7121fc7b90aa48f728240202bdb384
DvzZ <estebandavid578@gmail.com>
2024-09-30 02:12:30 +0200

    Add config

---
+++ .prettierrc.mjs
@@ -0,0 +1,40 @@
+/** @type {import("prettier").Config} */
+export default {
+	printWidth: 80,
+	semi: false,
+	singleQuote: false,
+	jsxSingleQuote: false,
+	quoteProps: "consistent",
+	tabWidth: 2,
+	trailingComma: "es5",
+	useTabs: true,
+	endOfLine: "lf",
+	arrowParens: "always",
+	plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+	overrides: [
+		{
+			files: ["*.json", "*.md", "*.toml", "*.yml"],
+			options: {
+				useTabs: false,
+			},
+		},
+		{
+			files: "*.astro",
+			options: {
+				parser: "astro",
+			},
+		},
+		{
+			files: "*.tsx",
+			options: {
+				parser: "typescript",
+			},
+		},
+		{
+			files: "*.ts",
+			options: {
+				parser: "typescript",
+			},
+		},
+	],
+}
