28a80ee53990d889a8fc28ab77b1edeccdd1c89a
DvzZ <estebandavid578@gmail.com>
2024-09-30 04:08:28 +0200

    Update tailwind.config.mjs with extended font family options

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				kaushan: ["Kaushan", "sans-serif"],
+				spacemono: ["Spacemono", "monospace"],
+				spacemonobold: ["SpacemonoBold", "monospace"],
+			},
+		},
 	},
 	plugins: [],
 }
