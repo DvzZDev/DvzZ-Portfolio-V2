8f0a8239ada4af41fcf43672882c43c46a4ae5e1
DvzZ <estebandavid578@gmail.com>
2024-10-01 06:02:57 +0200

    Refactor tailwind.config.mjs to enable JIT mode and add safelist for classes to prevent purging.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+	mode: "jit", // Activar JIT
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
@@ -11,4 +12,17 @@ export default {
 		},
 	},
 	plugins: [],
+	safelist: [
+		// Lista de clases para evitar que se purguen
+		"sm:col-start-1",
+		"sm:col-start-2",
+		"sm:text-left",
+		"sm:text-right",
+		"sm:row-start-1",
+		"sm:row-start-2",
+		"sm:row-start-3",
+		"sm:block",
+		"sm:flex",
+		"sm:justify-center",
+	],
 }
